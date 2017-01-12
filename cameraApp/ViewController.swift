f88458a5826a96c8a867604676bc30092f0c7ad5
ikutahikaru <hikaruikuta@hanae-no-MacBook-Pro.local>
2017-01-12 11:44:07 +0900

    first commit

---
+++ cameraApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cameraApp
+//
+//  Created by Hikaru Ikuta on 2017/01/12.
+//  Copyright © 2017年 Hikaru Ikuta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
